7e104f2f46e47a9512dcc575880d3157c8d94b74
Matteo Besio <matteo.besio@gmail.com>
2020-12-30 22:41:21 -0800

    completed form to capture user inputs

--- client/src/components/form.jsx
+++ client/src/components/form.jsx
@@ -1,25 +1,56 @@
 import React from 'react'
 import { Button, Checkbox, Form } from 'semantic-ui-react'
 
+class Modulo extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      importo: 0,
+      tasso: 2.0,
+      durata: 30
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('the value has been clicked e', e.target);
+    console.log('this.state', this.state);
+  }
 
+  handleInputChange(e) {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
 
-const Modulo = () => (
-  <Form style={{margin: '4em'}} >
-    <Form.Field>
-      <label>Importo (Amount) (€)</label>
-      <input placeholder='200.000' />
-    </Form.Field>
-    <Form.Field>
-      <label>Tasso d'interesse (Interest Rate) (%)</label>
-      <input placeholder='2,0%' />
-    </Form.Field>
-    <Form.Field>
-      <label>Durata (Duration) (Anni)</label>
-      <input placeholder='30' />
-    </Form.Field>
-    <Button content='Calcola Mutuo' primary type='submit'/>
-  </Form>
-)
+  render() {
+    return(
+      <Form style={{margin: '4em', marginRight: '20em'}} onSubmit={this.handleSubmit}>
+        <Form.Field>
+          <label>Importo (Amount) (€)</label>
+            <input
+              name='importo'
+              placeholder='200.000'
+              onChange={this.handleInputChange}/>
+        </Form.Field>
+        <Form.Field>
+          <label>Tasso d'interesse (Interest Rate) (%)</label>
+            <input placeholder='2,0%'
+              name='tasso'
+              onChange={this.handleInputChange}/>
+        </Form.Field>
+        <Form.Field>
+          <label>Durata (Duration) (Anni)</label>
+            <input placeholder='30'
+              name='durata'
+              onChange={this.handleInputChange}/>
+        </Form.Field>
+        <Button content='Calcola Mutuo' primary type='submit'/>
+      </Form>
+    )
+  }
+}
 
 export default Modulo
