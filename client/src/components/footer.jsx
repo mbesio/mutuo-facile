3cc4bc91383731a594dd39a83862ab91e6935aa4
Matteo Besio <matteo.besio@gmail.com>
2021-01-02 20:53:20 -0800

    completed hover functionality for feedback link

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -7,8 +7,18 @@ class Footer extends React.Component{
     this.state ={
       isHover: false
     }
+    this.toggleFeedback = this.toggleFeedback.bind(this);
   }
+
+  toggleFeedback() {
+    console.log('hello')
+    this.setState({
+      isHover: !this.state.isHover
+    })
+  }
+
   render() {
+    var feedbackButtonStyle = this.state.isHover ? {textAlign: 'center', color: '#e0edff'} : {textAlign: 'center', color: 'white'}
     return (
       <div style={{padding: '1.0em', backgroundColor: 'black', color: 'white', position: 'sticky', bottom: '0', zIndex: '1'}}>
         <div style={{display: 'flex', justifyContent: 'space-between'}}>
@@ -17,7 +27,7 @@ class Footer extends React.Component{
             <RiHomeGearFill style={{fontSize: '100%'}} />
             <div style={{ fontSize: '100%', paddingLeft: '0.5em'}}>Mutuo Facile</div>
           </div>
-        <a target={"_blank"} rel={"noopener noreferrer"} style={{textAlign: 'center', color: 'white'}} href={"https://forms.gle/pGj8ZZv3P9NEfV8E9"}>Feedback e suggerimenti</a>
+        <a target={"_blank"} rel={"noopener noreferrer"} style={feedbackButtonStyle} href={"https://forms.gle/pGj8ZZv3P9NEfV8E9"} onMouseEnter={this.toggleFeedback} onMouseLeave={this.toggleFeedback}>Feedback e suggerimenti</a>
         <div style={{alignItems: 'right'}}>Versione Beta</div>
         </div>
       </div>
