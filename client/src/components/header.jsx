15a9dad81a8faa7babfb359d3081d6b420022d4a
Matteo Besio <matteo.besio@gmail.com>
2021-01-02 19:24:27 -0800

    added logo

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { RiHomeGearFill } from 'react-icons/ri';
 
 var Header = () => {
   return (
     <div>
 
       <div style={{padding: '2.0em', backgroundColor: '#2485D0', color: 'white', textAlign: 'center', position: 'sticky', top: '0', zIndex: '1'}}>
+        <RiHomeGearFill style={{fontSize: '300%'}} />
 
         <div style={{ fontSize: '200%'}}>Mutuo Facile</div>
         <div style={{paddingTop: '0.5em', fontStyle: 'italic'}}>Calcola la rata del mutuo</div>
