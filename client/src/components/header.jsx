aa979629fb9e8958d41de6e34703f5e02712d7e9
Matteo Besio <matteo.besio@gmail.com>
2021-01-02 18:55:24 -0800

    added subtitle

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 var Header = () => {
   return (
     <div>
-      <div style={{ fontSize: '200%', textAlign: 'center', padding: '1.0em', backgroundColor: '#2485D0', color: 'white', position: 'sticky', top: '0', zIndex: '1'}}>Mutuo Facile</div>
+
+      <div style={{padding: '2.0em', backgroundColor: '#2485D0', color: 'white', textAlign: 'center', position: 'sticky', top: '0', zIndex: '1'}}>
+
+        <div style={{ fontSize: '200%'}}>Mutuo Facile</div>
+        <div style={{paddingTop: '0.5em', fontStyle: 'italic'}}>Calcola la rate del mutuo</div>
+      </div>
+
     </div>
+
   )
 }
 
