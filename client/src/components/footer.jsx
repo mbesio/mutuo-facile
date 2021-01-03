0054e38d2db4e59ebafbb929f8c61e6ba777b5fd
Matteo Besio <matteo.besio@gmail.com>
2021-01-02 20:37:54 -0800

    completed footer design

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { RiHomeGearFill } from 'react-icons/ri';
+
+var Footer = () => {
+  return (
+    <div style={{padding: '1.0em', backgroundColor: 'black', color: 'white', position: 'sticky', bottom: '0', zIndex: '1'}}>
+      <div style={{display: 'flex', justifyContent: 'space-between'}}>
+        <div style={{display: 'flex', alignItems: 'center'}}>
+          <div style={{paddingRight: '0.75em'}}>©</div>
+          <RiHomeGearFill style={{fontSize: '100%'}} />
+          <div style={{ fontSize: '100%', paddingLeft: '0.5em'}}>Mutuo Facile</div>
+        </div>
+      <a target={"_blank"} rel={"noopener noreferrer"} style={{textAlign: 'center', color: 'white'}} href={"https://forms.gle/pGj8ZZv3P9NEfV8E9"}>Feedback e suggerimenti</a>
+      <div style={{alignItems: 'right'}}>Versione Beta</div>
+      </div>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Footer;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 
 import Header from './components/header.jsx';
 import Modulo from './components/form.jsx';
+import Footer from './components/footer.jsx';
 
 var App = () => {
   return(
     <div>
       <Header/>
       <Modulo/>
+      <Footer/>
     </div>
   )
 }
