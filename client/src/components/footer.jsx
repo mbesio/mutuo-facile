0ff8a871c93412f8ab51fa0ffec94631a1c0780e
Matteo Besio <matteo.besio@gmail.com>
2021-01-02 20:41:20 -0800

    refactored footer to a class component

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import { RiHomeGearFill } from 'react-icons/ri';
 
-var Footer = () => {
-  return (
-    <div style={{padding: '1.0em', backgroundColor: 'black', color: 'white', position: 'sticky', bottom: '0', zIndex: '1'}}>
-      <div style={{display: 'flex', justifyContent: 'space-between'}}>
-        <div style={{display: 'flex', alignItems: 'center'}}>
-          <div style={{paddingRight: '0.75em'}}>©</div>
-          <RiHomeGearFill style={{fontSize: '100%'}} />
-          <div style={{ fontSize: '100%', paddingLeft: '0.5em'}}>Mutuo Facile</div>
+class Footer extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state ={
+      isHover: false
+    }
+  }
+  render() {
+    return (
+      <div style={{padding: '1.0em', backgroundColor: 'black', color: 'white', position: 'sticky', bottom: '0', zIndex: '1'}}>
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+          <div style={{display: 'flex', alignItems: 'center'}}>
+            <div style={{paddingRight: '0.75em'}}>©</div>
+            <RiHomeGearFill style={{fontSize: '100%'}} />
+            <div style={{ fontSize: '100%', paddingLeft: '0.5em'}}>Mutuo Facile</div>
+          </div>
+        <a target={"_blank"} rel={"noopener noreferrer"} style={{textAlign: 'center', color: 'white'}} href={"https://forms.gle/pGj8ZZv3P9NEfV8E9"}>Feedback e suggerimenti</a>
+        <div style={{alignItems: 'right'}}>Versione Beta</div>
         </div>
-      <a target={"_blank"} rel={"noopener noreferrer"} style={{textAlign: 'center', color: 'white'}} href={"https://forms.gle/pGj8ZZv3P9NEfV8E9"}>Feedback e suggerimenti</a>
-      <div style={{alignItems: 'right'}}>Versione Beta</div>
       </div>
-
-
-
-
-
-    </div>
-  )
+    )
+  }
 }
 
 export default Footer;
