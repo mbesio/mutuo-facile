7727b4f09eb55c542c169e622fcf58e563904d63
Matteo Besio <matteo.besio@gmail.com>
2021-01-08 13:47:02 -0800

    improved formatting of the form component

--- client/src/components/form.jsx
+++ client/src/components/form.jsx
@@ -55,11 +55,11 @@ class Modulo extends React.Component {
   render() {
     return(
       <div>
-        <div style={{padding: '1.0em', backgroundColor: 'white'}}></div>
-        <div style={{display: 'flex', justifyContent: 'center'}}>
+        <div style={{paddingTop: '1.0em', backgroundColor: 'white'}}></div>
+        <div style={{display: 'flex', justifyContent: 'center', marginLeft: '5%', marginRight: '5%'}}>
           <div>
-            <div style={{fontSize: '200%', paddingBottom: '5px', marginTop: '1em', marginRight: '4em', marginLeft: '2.8em'}}>Modifica parametri mutuo</div>
-            <Form style={{marginTop: '1.5em', marginBottom: '4em', marginLeft: '4em', marginRight: '10em'}} onSubmit={this.handleSubmit}>
+            <div style={{fontSize: '200%', paddingBottom: '5px', marginRight: '5%'}}>Modifica parametri mutuo</div>
+            <Form style={{marginTop: '1.5em', marginBottom: '4em', marginRight: '10em'}} onSubmit={this.handleSubmit}>
               <Form.Field>
                 <label>Importo (€)</label>
                   <input
