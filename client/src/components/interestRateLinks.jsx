20b5e1fabeebf4901847717e8f09c9b2e780315c
Matteo Besio <matteo.besio@gmail.com>
2021-04-24 11:43:12 -0700

    chart with euribor time series correctly displaying in the front end

--- client/src/components/interestRateLinks.jsx
+++ client/src/components/interestRateLinks.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Line } from 'react-chartjs-2';
+import axios from 'axios';
 
 const InterestRateLinks = () => {
+  const [euriborTimeSeries, setEuriborTimeSeries] = useState({});
+
+  useEffect(() => {
+    axios.get('/euribor')
+      .then( (updatedEuriborTimeSeries) => {
+        setEuriborTimeSeries(updatedEuriborTimeSeries.data);
+      })
+      .catch((err) => {
+        console.log('there was an error');
+      })
+  },[]);
+
+
   return (
     <div>
       Here we will have links to InterestRates!
+      <div style={{width: "80%"}}>
+      <Line
+        data ={{
+          labels: euriborTimeSeries.dates,
+          datasets: [
+            {
+              label: 'Euribor 3m rate (%)',
+              data: euriborTimeSeries.prices
+            }
+          ]
+        }}
+        options ={{
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: false
+                  }
+              }]
+          }
+      }}
+      />
+    </div>
+
     </div>
   )
 }
