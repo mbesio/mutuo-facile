618eaf5185380fe213f9cbdb4c0b7b89bd2ada92
Matteo Besio <matteo.besio@gmail.com>
2021-04-26 16:17:14 -0700

    completed redis cache and CRON job

--- server/index.js
+++ server/index.js
@@ -5,30 +5,52 @@ const port = process.env.PORT || 3010;
 const { QUANDL_API_KEY } = require('./quandlKey.js');
 const { parseEuribor } = require('./helpers/parseEuribor.js')
 const axios = require('axios');
-const path = require('path');
-
+const path = require('path'); // may be able to remove this
+const redis = require('redis');
 
 app.use(express.static('client/dist'));
 
+const client = redis.createClient();
+
+
+// set up a cron job to erase the cache on the first day of every month
+var CronJob = require('cron').CronJob;
+var job = new CronJob('00 00 00 1 * *', function() {
+  // erase the redis cache
+  client.flushdb( function (err, succeeded) {
+    console.log(succeeded); // will be OK if successfull
+  });
+}, null, true, 'America/Los_Angeles');
+job.start();
+
 
 app.get('/euribor', (req, res) => {
-  axios({
-    method: 'get',
-    url: 'https://www.quandl.com/api/v3/datasets/ECB/RTD_M_S0_N_C_EUR3M_E.json',
-    params: {
-      api_key: QUANDL_API_KEY,
-      start_date: '2010-01-01',
-      order: 'asc'
+
+  client.get('euribor', (err, redisReply) => {
+    if (redisReply) {
+      res.status(200).send(JSON.parse(redisReply));
+    } else {
+      axios({
+        method: 'get',
+        url: 'https://www.quandl.com/api/v3/datasets/ECB/RTD_M_S0_N_C_EUR3M_E.json',
+        params: {
+          api_key: QUANDL_API_KEY,
+          start_date: '2010-01-01',
+          order: 'asc'
+        }
+      })
+       .then((data) => {
+         const timeSeries = parseEuribor(data);
+         client.set('euribor', JSON.stringify(timeSeries));
+         res.status(200).send(timeSeries);
+       })
+       .catch((err) => {
+         console.log('there was an error', err);
+         res.status(400).send();
+       })
     }
   })
-   .then((data) => {
-     const timeSeries = parseEuribor(data);
-     res.status(200).send(timeSeries);
-   })
-   .catch((err) => {
-     console.log('there was an error', err);
-     res.status(400).send();
-   })
+
 })
 
 app.get('/*', function(req, res) {
