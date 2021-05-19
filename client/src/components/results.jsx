56da55944721225d8e2d5067728c5eccb2f8c018
Matteo Besio <matteo.besio@gmail.com>
2021-05-18 20:52:48 -0700

    refactored css out of results component

--- client/src/components/results.jsx
+++ client/src/components/results.jsx
@@ -25,33 +25,33 @@ var Results = (props) => {
         <div className="home-results-inner-inner-wrapper">
           <div className="home-results-column-wrapper">
             <div className="home-results-subtitle">Parametri</div>
-            <div style={{display: 'flex', alignItems: 'center'}}>
+            <div className="home-results-parameters">
                 <AiFillBank style={{margin: '0.4em'}} />
                 <NumberFormat value={props.mutuo.importo} displayType={'text'} thousandSeparator={true} prefix={'€'} />
               </div>
-              <div style={{display: 'flex', alignItems: 'center'}}>
+              <div className="home-results-parameters">
                 <AiOutlinePercentage style={{margin: '0.4em'}} />
                 <div>{props.mutuo.tasso}%</div>
               </div>
-              <div style={{display: 'flex', alignItems: 'center'}}>
+              <div className="home-results-parameters">
                 <ImCalendar style={{margin: '0.4em'}}/>
                 <div>{props.mutuo.durata} anni</div>
               </div>
             </div>
-            <div style={{display:'flex', flexDirection: 'column', justifyContent: 'flex-start', marginTop: '1%', marginBottom: '1%', marginRight: '2%'}}>
-              <div style={{fontSize: '125%', marginBottom: '15px'}}>Risultato</div>
-              <div style={{display: 'flex', justifyContent: 'space-between',alignItems: 'center'}}>
+            <div className="home-results-column-wrapper">
+              <div className="home-results-subtitle">Risultato</div>
+              <div className="home-results-parameters">
                 <div style={{margin: '0.4em'}}>Rata:</div>
                 <NumberFormat style={{fontSize: '200%'}} value={installment} displayType={'text'} thousandSeparator={true} prefix={'€'} />
               </div>
-              <div style={{display: 'flex', justifyContent: 'space-between',alignItems: 'center'}}>
+              <div className="home-results-parameters">
                 <div style={{margin: '0.4em'}}>
                   <div>Costo totale</div>
                   <div style={{fontSize: '75%'}}>(Capitale + Interessi)</div>
                 </div>
                 <NumberFormat value={totalCostDisplay} displayType={'text'} thousandSeparator={true} prefix={'€'} />
               </div>
-              <div style={{display: 'flex', justifyContent: 'space-between',alignItems: 'center'}}>
+              <div className="home-results-parameters">
                 <div style={{margin: '0.4em'}}>
                   <div> Quota interessi</div>
                   <div style={{fontSize: '75%'}}>(% totale costo mutuo)</div>
